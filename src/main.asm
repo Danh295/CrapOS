1b94e01a2847ac8430db5446bb762976474a6716
Danny Hu <hudanny295@gmail.com>
2024-06-19 04:47:07 -0400

    Updated main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,3 @@
-; this is the bootloader
-
 ; specify 'origin' of starting address to 0x7c00 (tells assembler where code is to be loaded)
 ; 0x7C00 is the hardcoded convention and addressfor the first sector of the bootable device to be loaded into memory
 org 0x7C00
@@ -8,8 +6,40 @@ org 0x7C00
 ; x86 should be backwards compatible with 8086 architectures, so must start with 16 bits */
 bits 16
 
+start:
+    jmp main ; ensures that main is still the entry point
+
+
+; prints a string to the screen
+; Params:
+;   - ds:si points to string
+; 
+puts:
+    ;save registeers we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb      ; loads the next character in al
+    or al, al  ; verify if the next cahracter is null?
+    jz .done
+
+.done:
+    pop ax
+    pop si
+
 main:
-    HLT ; stops CPU from executing (can be resumed with an interrupt)
+
+    ; setup data segments
+    mov ax, 0       ; can't write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00  ; stack grows downwards from where memory loads in, so start from beginning of operating system
+
+    hlt ; stops CPU from executing (can be resumed with an interrupt)
 
 .halt:
     jmp .halt ; jumpts to .halt unconditionally to form infinite loop if CPU starts again (can also juse use 'jmp $')
